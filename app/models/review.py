a7a58d5337a1bc77968f440ec3ecfa0f72204956
Tony <tr33947@gmail.com>
2020-11-26 21:21:24 +0300

    made changes

--- app/models/review.py
+++ app/models/review.py
@@ -1,7 +1,16 @@
 class Review:
 
     all_reviews = []
+    @classmethod
+    def get_reviews(cls,id):
 
+        response = []
+
+        for review in cls.all_reviews:
+            if review.movie_id == id:
+                response.append(review)
+
+        return response
     def __init__(self,movie_id,title,imageurl,review):
         self.movie_id = movie_id
         self.title = title
--- app/request.py
+++ app/request.py
@@ -30,30 +30,6 @@ def get_movies(category):
 
     return movie_results
 
-#  def process_results(movie_list):
-#     '''
-#     Function  that processes the movie result and transform them to a list of Objects
-
-#     Args:
-#         movie_list: A list of dictionaries that contain movie details
-
-#      Returns :
-#         movie_results: A list of movie objects
-#     '''
-#     movie_results = []
-#     for movie_item in movie_list:
-#         id = movie_item.get('id')
-#         title = movie_item.get('original_title')
-#         overview = movie_item.get('overview')
-#         poster = movie_item.get('poster_path')
-#         vote_average = movie_item.get('vote_average')
-#         vote_count = movie_item.get('vote_count')
-
-#         if poster:
-#             movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
-#             movie_results.append(movie_object)
-
-#     return movie_results
 
 def process_results(movie_list):
     '''
@@ -97,6 +73,7 @@ def get_movie(id):
             movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
 
     return movie_object 
+    
 def search_movie(movie_name):
     search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
     with urllib.request.urlopen(search_movie_url) as url:
